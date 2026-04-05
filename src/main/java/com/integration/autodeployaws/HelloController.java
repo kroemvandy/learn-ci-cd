c95aac59bf5fb429e66dc6ec3bae9b3b4e10f0a6
Kroem Vandy <kroem123vandy@gmail.com>
2026-04-05 21:37:42 +0700

    Change return string

--- src/main/java/com/integration/autodeployaws/HelloController.java
+++ src/main/java/com/integration/autodeployaws/HelloController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String hello() {
-        return "Hello world";
+        return "Hello world CI/CD";
     }
 }
